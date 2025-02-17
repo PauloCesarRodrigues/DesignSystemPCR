2df39266f2a7c08e6c3e6253901663638b1b14e9
Paulo Cesar Rodrigues <pauloceasrrtv321@gmail.com>
2025-02-17 10:25:01 -0300

    fix: add ref to textInput

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -7,3 +7,5 @@ export * from './components/TextInput/index'
 export * from './components/TextArea'
 export * from './components/Checkbox/index'
 export * from './components/MultiStep/index'
+
+export * from './styles'
